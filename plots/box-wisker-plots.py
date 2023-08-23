fd497a66490271f5e111c11d7f04a157609a81dc
Nadine Randel <nadine@voodoo-arts.net>
2023-08-23 18:43:35 +0100

    change from inch to um

--- plots/box-wisker-plots.py
+++ plots/box-wisker-plots.py
@@ -9,10 +9,14 @@ import csv
 # Import csv as panda dataframe
 
 import pandas as pd
-df = pd.read_csv("/Users/nadine/repos/statistics/data/size-Pd_June2023.csv") 
+df = pd.read_csv("/Users/nadine/repos/statistics/data/size-Pd_June2023-um.csv") 
 print(df) 
 
-# %%
+# Values in current dataframe are in some value from Fiji!!
 
-df.boxplot(vert=False)
+# %%
+# box and wisker plot with label on left side
+
+df.boxplot(vert=False, grid = False, color=dict(boxes='k', whiskers='k', medians='b', caps='k'))
+
 # %%
